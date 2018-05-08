c28ea8863ef488e1ceec3566ef09666795ad0c32
shuchen <shuchenowl@gmail.comm>
2018-05-08 12:15:07 -0500

    made chatbox

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
@@ -7,9 +8,21 @@ import About from './About';
 import Profile from './Profile';
 import Game from './LiveGame';
 
+=======
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Nav from "./Nav";
+import Home from "./Home";
+import Learn from "./Learn";
+import About from "./About";
+import Profile from "./Profile";
+import Game from "./LiveGame";
+import Chat from "./LiveGame/chat";
+>>>>>>> made chatbox
 const App = () => (
   <div id="page">
     <Nav />
+    <Chat />
     <div className="main">
       <Switch>
         <Route path="/learn" component={Learn} />
