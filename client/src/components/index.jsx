16585f8823a9405137d5362ed342a3506cd928ca
shuchen <shuchenowl@gmail.comm>
2018-05-08 14:56:20 -0500

    created lobby

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Nav from './Nav';
-import Home from './Home';
-import Learn from './Learn';
-import About from './About';
-import Profile from './Profile';
-import Game from './LiveGame';
-
-=======
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Nav from "./Nav";
@@ -18,20 +7,16 @@ import About from "./About";
 import Profile from "./Profile";
 import Game from "./LiveGame";
 import Chat from "./LiveGame/chat";
->>>>>>> made chatbox
 const App = () => (
   <div id="page">
     <Nav />
-    <Chat />
-    <div className="main">
-      <Switch>
-        <Route path="/learn" component={Learn} />
-        <Route path="/about" component={About} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/game" component={Game} />
-        <Route path="/" component={Home} />
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/learn" component={Learn} />
+      <Route path="/about" component={About} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/game" component={Game} />
+      <Route path="/" component={Home} />
+    </Switch>
   </div>
 );
 export default App;
